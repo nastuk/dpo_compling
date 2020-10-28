8b8ade1baf6f5f2272c37e103691d8dcfa96773d
nastuk <73242035+nastuk@users.noreply.github.com>
2020-10-28 18:21:45 +0500

    Add files via upload

---
+++ HW 14.10 N2.py
@@ -0,0 +1,8 @@
+a = 'Настя'
+b = "изучает"
+c = "Python"
+print(a, b, c, sep=" ")
+print(b, a, c, sep=" ")
+print(c, a, b, sep=" ")
+print(c, b, a, sep=" ")
+
---
+++ HW 14.10 N4.py
@@ -0,0 +1,4 @@
+text = "WOW,NOEL SEES LEON"
+text2=(text.lower())
+print(text2)
+print(text2[::-1])
